19c7c539436058d5a2c5ff88a9592cc648d033db
Kakhter <khalid.bharat@gmail.com>
2021-10-29 15:50:09 +0530

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace webapidocker.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing1", "Bracing1", "Chilly1", "Cool1", "Mil1", "Warm1", "Balmy1", "Hot1", "Sweltering1", "Scorching1"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
